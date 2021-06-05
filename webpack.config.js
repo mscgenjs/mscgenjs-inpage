db6f7ee58ab74489277e0bf868c40fcbe2c90e5e
sverweij <sverweij@users.noreply.github.com>
2021-06-05 15:34:58 +0200

    refactor: modernize a bit and render more uniformly

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
 
-module.exports = () => ({
+/** @type {import('webpack').Configuration} */
+const CONFIG = {
   mode: "production",
   entry: "./src/mscgen-inpage.js",
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "mscgen-inpage.js",
   },
-});
+};
+
+module.exports = () => CONFIG;
